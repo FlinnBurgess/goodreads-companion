b86f1d7ac0769619b6d0dc41be03a062bcd9ffc2
Flinn Burgess <flinn.burgess@gmail.com>
2020-09-01 19:52:16 +0100

    Include number of pages criterion on recommendations page

--- lib/recommendations.dart
+++ lib/recommendations.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:goodreads_companion/book.dart';
 
 class BookRecommendationsPage extends StatefulWidget {
@@ -16,17 +17,53 @@ class BookRecommendationsPage extends StatefulWidget {
 class _BookRecommendationsPageState extends State<BookRecommendationsPage> {
   Book selectedBook;
   var random = Random();
+  int maxPages;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
+        Row(
+          children: [
+            Container(
+                width: 300,
+                child: TextField(
+                  decoration: new InputDecoration(labelText: "Max pages"),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                  ],
+                  onChanged: (input) {
+                    if (input == '') {
+                      print('Empty');
+                      setState(() => maxPages = null);
+                    }
+                    setState(() => maxPages = num.parse(input));
+                  },
+                ))
+          ],
+        ),
         FlatButton(
           child: Text('Random book'),
-          onPressed: () => setState(() =>
-              selectedBook = widget.books[random.nextInt(widget.books.length)]),
+          onPressed: () {
+            List<Book> booksToSearch = List.from(widget.books);
+            if (maxPages != null) {
+              booksToSearch = booksToSearch
+                  .where((book) =>
+              book.numberOfPages != null &&
+                  book.numberOfPages < maxPages)
+                  .toList();
+            }
+            setState(() =>
+            selectedBook =
+            booksToSearch[random.nextInt(booksToSearch.length)]);
+          },
         ),
-        selectedBook == null ? Container() : Text(selectedBook.title)
+        selectedBook == null
+            ? Text('No matches found')
+            : Text(
+            '${selectedBook.title}, number of pages: ${selectedBook
+                .numberOfPages}')
       ],
     );
   }
