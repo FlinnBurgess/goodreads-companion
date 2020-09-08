8a5e20e8ac053450763c35fe75d256780c7d4bed
Flinn Burgess <flinn.burgess@gmail.com>
2020-09-08 08:53:29 +0100

    Show book display on tapping a book in the book list

--- lib/book_display.dart
+++ lib/book_display.dart
@@ -22,42 +22,56 @@ class BookDisplay extends StatelessWidget {
         child: Card(
           child: Padding(
               padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.network(book.imageUrl),
-                  SizedBox(width: 10,),
-                  Flexible(
-                      child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        book.title,
-                        style: TextStyle(fontSize: 18),
-                      ),
-                      Text(book.author, style: TextStyle(fontSize: 13, color: Colors.grey[600]),),
-                      SizedBox(height: 5,),
-                      RatingBarIndicator(
-                        rating: book.averageRating,
-                        itemBuilder: (_, __) => Icon(
-                          Icons.star,
-                          color: Colors.amber,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.network(book.imageUrl),
+                        SizedBox(
+                          width: 10,
                         ),
-                        itemCount: 5,
-                        itemSize: 20,
-                      ),
-                      SizedBox(height: 5,),
-                      book.numberOfPages == null
-                          ? null
-                          : Text('Pages: ${book.numberOfPages}'),
-                      daysToRead == null
-                          ? null
-                          : Text(
-                              'Estimated time to read: $daysToRead ${daysToRead > 1 ? 'days' : 'day'}.')
-                    ].where((element) => element != null).toList(),
-                  ))
-                ],
-              )),
+                        Flexible(
+                            child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              book.title,
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            Text(
+                              book.author,
+                              style: TextStyle(
+                                  fontSize: 13, color: Colors.grey[600]),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            RatingBarIndicator(
+                              rating: book.averageRating,
+                              itemBuilder: (_, __) => Icon(
+                                Icons.star,
+                                color: Colors.amber,
+                              ),
+                              itemCount: 5,
+                              itemSize: 20,
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            book.numberOfPages == null
+                                ? null
+                                : Text('Pages: ${book.numberOfPages}'),
+                            daysToRead == null
+                                ? null
+                                : Text(
+                                    'Estimated time to read: $daysToRead ${daysToRead > 1 ? 'days' : 'day'}.')
+                          ].where((element) => element != null).toList(),
+                        ))
+                      ],
+                    )
+                  ])),
         ));
   }
 }
--- lib/main_ui.dart
+++ lib/main_ui.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:goodreads_companion/book_display.dart';
 import 'package:goodreads_companion/settings_page.dart';
 import 'package:goodreads_companion/statistics.dart';
 import 'package:provider/provider.dart';
@@ -20,89 +21,100 @@ class _MainUIState extends State<MainUI> {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<Library>(builder: (context, library, _) {
-      var tabs = library.shelves.keys
-          .map((shelfName) => Tab(
-        text: shelfName,
-      ))
-          .toList();
+    return Consumer<Library>(
+      builder: (context, library, _) {
+        var tabs = library.shelves.keys
+            .map((shelfName) => Tab(
+                  text: shelfName,
+                ))
+            .toList();
 
-      var tabPages = library.shelves.keys.map((shelfName) {
-        var books = library.shelves[shelfName].books;
-        switch (_selectedPage) {
-          case BOOKS_LIST:
-            return ListView.builder(
-                itemCount: books.length,
-                itemBuilder: (_, index) => Container(
-                  height: 50,
-                  child: Text(books[index].title),
-                ));
-          case STATISTICS:
-            return SingleChildScrollView(
-                child: Column(children: [
-                  NumberOfBooksStatistic(
-                    books: books,
-                  ),
-                  BooksReadStatistic(
-                    books: books,
-                  ),
-                  AverageNumberOfPagesStatistic(
-                    books: books,
-                  ),
-                  TotalPagesReadStatistic(
-                    books: books,
-                  ),
-                  AverageTimeToReadStatistic(
-                    books: books,
-                  ),
-                  AverageReadingRateStatistic(
-                    books: books,
-                  ),
-                  StartedReadingDaysStatistic(
-                    books: books,
-                  ),
-                  FinishReadingDaysStatistic(books: books),
-                ]));
-          case RECOMMEND:
-            return RandomBookPage(
-                books, library.shelves['read'].books);
-          case SETTINGS:
-            return SettingsPage();
-          default:
-            return Text('Number of books: ${books.length}');
-        }
-      }).toList();
+        var tabPages = library.shelves.keys.map((shelfName) {
+          var books = library.shelves[shelfName].books;
+          switch (_selectedPage) {
+            case BOOKS_LIST:
+              return ListView.builder(
+                  itemCount: books.length,
+                  itemBuilder: (_, index) => GestureDetector(
+                      onTap: () => showDialog(
+                          context: context,
+                          builder: (context) => Center(
+                                  child: Container(
+                                height: 250,
+                                child: BookDisplay(
+                                  book: books[index],
+                                ),
+                              ))),
+                      child: Center(
+                          child: Padding(padding: EdgeInsets.only(top: 30), child: Container(
+                        child: Text(books[index].title, textAlign: TextAlign.center,),
+                      )))));
+            case STATISTICS:
+              return SingleChildScrollView(
+                  child: Column(children: [
+                NumberOfBooksStatistic(
+                  books: books,
+                ),
+                BooksReadStatistic(
+                  books: books,
+                ),
+                AverageNumberOfPagesStatistic(
+                  books: books,
+                ),
+                TotalPagesReadStatistic(
+                  books: books,
+                ),
+                AverageTimeToReadStatistic(
+                  books: books,
+                ),
+                AverageReadingRateStatistic(
+                  books: books,
+                ),
+                StartedReadingDaysStatistic(
+                  books: books,
+                ),
+                FinishReadingDaysStatistic(books: books),
+              ]));
+            case RECOMMEND:
+              return RandomBookPage(books, library.shelves['read'].books);
+            case SETTINGS:
+              return SettingsPage();
+            default:
+              return Text('Number of books: ${books.length}');
+          }
+        }).toList();
 
-      return DefaultTabController(
-        length: tabs.length,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('Goodreads Companion'),
-            bottom: TabBar(isScrollable: true, tabs: tabs),
+        return DefaultTabController(
+          length: tabs.length,
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text('Goodreads Companion'),
+              bottom: TabBar(isScrollable: true, tabs: tabs),
+            ),
+            body: TabBarView(
+              children: tabPages,
+            ),
+            bottomNavigationBar: BottomNavigationBar(
+              backgroundColor: Colors.black,
+              selectedItemColor: Colors.amber[800],
+              unselectedItemColor: Colors.grey[800],
+              showUnselectedLabels: true,
+              items: [
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.library_books), title: Text('Book lists')),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.insert_chart), title: Text('Statistics')),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.repeat), title: Text('Random Book')),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.settings), title: Text('Settings'))
+              ],
+              currentIndex: _selectedPage,
+              onTap: (index) => setState(() => _selectedPage = index),
+            ),
           ),
-          body: TabBarView(
-            children: tabPages,
-          ),
-          bottomNavigationBar: BottomNavigationBar(
-            backgroundColor: Colors.black,
-            selectedItemColor: Colors.amber[800],
-            unselectedItemColor: Colors.grey[800],
-            showUnselectedLabels: true,
-            items: [
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.library_books), title: Text('Book lists')),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.insert_chart), title: Text('Statistics')),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.repeat), title: Text('Random Book')),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.settings), title: Text('Settings'))
-            ],
-            currentIndex: _selectedPage,
-            onTap: (index) => setState(() => _selectedPage = index),
-          ),
-        ),
-      );
-    },);
+        );
+      },
+    );
   }
 }
