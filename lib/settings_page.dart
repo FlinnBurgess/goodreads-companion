8aa419575136e4b8a625a07e014c70f4ca595fe0
Flinn Burgess <flinn.burgess@gmail.com>
2020-09-13 17:56:16 +0100

    Restyle settings page

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -17,21 +17,33 @@ class _SettingsPageState extends State<SettingsPage> {
     return Consumer3<User, Library, Settings>(
       builder: (context, user, library, settings, _) {
         return SingleChildScrollView(
-          child: Column(
-            children: [
-              Row(
+          child: Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: MediaQuery.of(context).size.width * 0.05,
+                  vertical: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text('Current user: ${user.userId}'),
-                  RaisedButton(
-                    child: Text('Change user'),
-                    onPressed: () =>
-                        _confirmUserIdChange(context, user, library),
-                  )
-                ],
-              ),
-              Row(
-                children: [
-                  Text('Select your country: '),
+                  Text('Current User'),
+                  Row(
+                    children: [
+                      Text('${user.userId}'),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      RaisedButton(
+                        child: Text('Change user'),
+                        onPressed: () =>
+                            _confirmUserIdChange(context, user, library),
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50))),
+                      )
+                    ],
+                  ),
+                  Divider(),
+                  Text('Country'),
                   CountryCodePicker(
                     onChanged: (selected) =>
                         settings.selectedCountry = selected.code,
@@ -39,51 +51,88 @@ class _SettingsPageState extends State<SettingsPage> {
                     favorite: ['GB', 'US'],
                     showCountryOnly: true,
                     showOnlyCountryWhenClosed: true,
-                  )
+                  ),
+                  Divider(),
+                  Text('Store buttons to show'),
+                  Wrap(
+                    crossAxisAlignment: WrapCrossAlignment.center,
+                    children: [
+                      Container(
+                          child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Checkbox(
+                            value: settings.showAmazonLink,
+                            onChanged: (value) =>
+                                settings.showAmazonLink = value,
+                          ),
+                          Text('Amazon'),
+                        ],
+                      )),
+                      SizedBox(
+                        width: 40,
+                      ),
+                      Container(
+                          child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Checkbox(
+                            value: settings.showAudibleLink,
+                            onChanged: (value) =>
+                                settings.showAudibleLink = value,
+                          ),
+                          Text('Audible'),
+                        ],
+                      )),
+                      SizedBox(
+                        width: 40,
+                      ),
+                      Container(
+                          child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Checkbox(
+                            value: settings.showGoogleBooksLink,
+                            onChanged: (value) =>
+                                settings.showGoogleBooksLink = value,
+                          ),
+                          Text('Google Books'),
+                        ],
+                      )),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Container(
+                          child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Checkbox(
+                            value: settings.showBookDepositoryLink,
+                            onChanged: (value) =>
+                                settings.showBookDepositoryLink = value,
+                          ),
+                          Text('Book Depository'),
+                        ],
+                      )),
+                    ],
+                  ),
+                  Divider(),
+                  Row(
+                    children: [
+                      Checkbox(
+                        value: settings.excludeBooksReadInASingleDayFromStats,
+                        onChanged: (value) => settings
+                            .excludeBooksReadInASingleDayFromStats = value,
+                      ),
+                      Flexible(
+                          child: Text(
+                        'Exclude books read in a single day from your statistics?\n(Prevents skew from misentered reading dates)',
+                        textAlign: TextAlign.center,
+                      )),
+                    ],
+                  ),
                 ],
-              ),
-              Row(
-                children: [
-                  Checkbox(
-                    value: settings.excludeBooksReadInASingleDayFromStats,
-                    onChanged: (value) =>
-                        settings.excludeBooksReadInASingleDayFromStats = value,
-                  ),
-                  Text(
-                      'Exclude books read in a single day from your statistics? This setting provides more accurate statistics if you know you have misentered reading dates.'),
-                ],
-              ),
-              Text('Store buttons to show'),
-              Row(
-                children: [
-                  Checkbox(
-                    value: settings.showAmazonLink,
-                    onChanged: (value) => settings.showAmazonLink = value,
-                  ),
-                  Text('Amazon'),
-                  Checkbox(
-                    value: settings.showAudibleLink,
-                    onChanged: (value) => settings.showAudibleLink = value,
-                  ),
-                  Text('Audible'),
-                ],
-              ),
-              Row(
-                children: [
-                  Checkbox(
-                    value: settings.showGoogleBooksLink,
-                    onChanged: (value) => settings.showGoogleBooksLink = value,
-                  ),
-                  Text('Google Books'),
-                  Checkbox(
-                    value: settings.showBookDepositoryLink,
-                    onChanged: (value) => settings.showBookDepositoryLink = value,
-                  ),
-                  Text('Book Depository'),
-                ],
-              ),
-            ],
-          ),
+              )),
         );
       },
     );
